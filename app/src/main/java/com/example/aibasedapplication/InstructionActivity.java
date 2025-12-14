62a4134cabdbee611ba1625c1303f06e13078ff6
Momina Waqar <mominawaqar30@gmail.com>
2025-12-13 19:10:20 -0800

    Update activity_home_page

--- app/src/main/java/com/example/aibasedapplication/InstructionActivity.java
+++ app/src/main/java/com/example/aibasedapplication/InstructionActivity.java
@@ -1,6 +1,9 @@
 package com.example.aibasedapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class InstructionActivity extends AppCompatActivity {
+    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,16 @@ public class InstructionActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            btn1 = findViewById(R.id.back_btn);
+
+            btn1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(InstructionActivity.this, Home_Page.class);
+                    startActivity(i);
+                }
+            });
             return insets;
         });
     }
